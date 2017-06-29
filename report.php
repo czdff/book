d31646ff7c00c23a405601c4be16843fb876d1ff
czdff <czdff@i.ua>
2017-06-30 00:43:50 +0300

    second chapter is finished and i will have weekend per 3 days=)

--- report.php
+++ report.php
@@ -1,6 +1,7 @@
 <h2>Сообщение о пхищении</h2>
 <?php
-$name = filter_input(INPUT_POST, 'firstname') . ' ' . filter_input(INPUT_POST, 'lastname');
+$first_name = filter_input(INPUT_POST, 'firstname');
+$last_name = filter_input(INPUT_POST, 'lastname');
 $when_it_happend = filter_input(INPUT_POST, 'whenithappend');
 $how_long = filter_input(INPUT_POST, 'howlong');
 $what_they_did = filter_input(INPUT_POST, 'whattheydid');
@@ -9,11 +10,11 @@ $other = filter_input(INPUT_POST, 'other');
 $aliendescription = filter_input(INPUT_POST, 'aliendescription');
 $fang_spoted = filter_input(INPUT_POST, 'fangspoted');
 $email = filter_input(INPUT_POST, 'email');
-$email=filter_input(INPUT_POST,'email');
-$msg = $name.' был похищен '.$when_it_happend.' иотсутствовал в течении '.$how_long.'.<br/>Количество пришельцев: '.$how_many.'<br/>Описание космических пришельцев: '.$aliendescription.'<br/>Что они делали? '.$what_they_did.'<br/>Фенг замечен? '.$fang_spoted.'<br/>Дополнительная информация: '.$other.'.';
-$subject='Космические пришельцы пхищали меня - Сообщение  похищении';
-$to='czdff@i.ua';
-mail($to, $subject, $msg, 'From:'.$email);
+$msg = $first_name . ' ' . $last_name . ' был похищен ' . $when_it_happend . ' иотсутствовал в течении ' . $how_long . '.<br/>Количество пришельцев: ' . $how_many . '<br/>Описание космических пришельцев: ' . $aliendescription . '<br/>Что они делали? ' . $what_they_did . '<br/>Фенг замечен? ' . $fang_spoted . '<br/>Дополнительная информация: ' . $other . '.';
+$subject = 'Космические пришельцы пхищали меня - Сообщение  похищении';
+$to = 'czdff@i.ua';
+
+mail($to, $subject, $msg, 'From:' . $email);
 
 echo 'Спасибо за заполнение формы.<br/>'
     . 'Вы были похищены ' . $when_it_happend . ' и отсутствовали в течении ' . $how_long . '<br/>'
@@ -24,4 +25,13 @@ echo 'Спасибо за заполнение формы.<br/>'
     . 'Дополнительная информация: ' . $other . '<br/>'
     . 'Ваш адрес электронной почты: ' . $email;
 
+$dbc = mysqli_connect('localhost', 'root', '', 'aliendatabase')
+or die('Ошибка соединение с БД');
+mysqli_set_charset($dbc, 'utf8');
+$query = "INSERT INTO aliens_abduction (first_name, last_name, when_it_happened, how_long,
+how_many, alien_description, what_they_did, fang_spoted, other, email) VALUES ('$first_name','$last_name','$when_it_happend','$how_long','$how_many','$aliendescription','$what_they_did','$fang_spoted','$other','$email')";
 
+$result = mysqli_query($dbc, $query)
+or die ('Ошибка запроса');
+
+mysqli_close($dbc);
